f0263f76092df334f83f17a9c05e20ab402d8021
balghaith <balghaith05@gmail.com>
2026-09-15 19:44:45 -0700

    fixed app.py

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ HORIZONS_DAYS = [7, 14, 30]
 
 def sync_data():
     repo_dir = os.path.dirname(os.path.abspath(__file__))
+    token = os.environ.get("GITHUB_TOKEN")
+    remote_url = f"https://{token}@github.com/balghaith/YouTube-Forecast-Algorithm.git"
+    subprocess.run(["git", "remote", "set-url", "origin", remote_url], cwd=repo_dir)
     subprocess.run(["git", "fetch", "origin", "main"], cwd=repo_dir)
     subprocess.run(["git", "reset", "--hard", "origin/main"], cwd=repo_dir)
 
